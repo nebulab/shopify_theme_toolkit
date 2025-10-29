cb0a3f85f4114cd55014563c16a5c1b02816dbd6
Massimiliano Lattanzio <massimiliano.lattanzio@gmail.com>
2025-10-29 11:45:29 +0100

    Add logging helpers

--- install.sh
+++ install.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 set -eu
 
-echo "installation complete! 🎉"
+# Logging helpers
+info() {
+  echo "$@" >&2
+}
+
+error() {
+  echo "$(red "$@") ❌" >&2
+  exit 1
+}
+
+blue() {
+  printf "\033[34m%s\033[0m" "$*"
+}
+
+green() {
+  printf "\033[32m%s\033[0m" "$*"
+}
+
+red() {
+  printf "\033[31m%s\033[0m" "$*"
+}
+
+info "$(green "Installation complete!") 🎉"
